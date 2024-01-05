2d020e86d9a71bab05d6610addcb04f633cb8b96
Isaac Robbins <isaacprobbins@gmail.com>
2024-01-05 17:18:37 -0500

    Build with Tauri
    The website can now be installed as a desktop application

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: (process.env.CABOOSE_WEB_USE_EXPORT ?? "false") == "true" ? 'export' : undefined,
     experimental: {
         typedRoutes: true,
     }
