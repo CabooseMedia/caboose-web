cc9d145e3bb23a7db22c9390cd9f363c26e6396d
Isaac Robbins <isaacprobbins@gmail.com>
2024-01-04 19:05:08 -0500

    try and export request handler

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,9 @@ import { UNIVERSAL } from '@util/universal';
 
 logger.debug("All imports loaded successfully. Starting Caboose...");
 
+let caboose: CabooseServer;
+let ready = false;
+
 async function start() {
 
     logger.info("Welcome to Caboose! Getting things ready...");
@@ -17,12 +20,26 @@ async function start() {
     UNIVERSAL.CONTENT_DIR = path.resolve(UNIVERSAL.ROOT_DIR, 'content');
     UNIVERSAL.DATA_DIR = path.resolve(UNIVERSAL.ROOT_DIR, 'data');
 
-    const caboose = new CabooseServer();
+    caboose = new CabooseServer();
 
     await caboose.start();
 
     logger.info("Caboose is ready! Enjoy!");
 
+    ready = true;
 }
 
 start();
+
+export default function getNextRequestHandler() {
+    if (!ready) {
+        return (req: any, res: any) => {
+            res.status(503).json({
+                code: 503,
+                method: req.method,
+                message: `Caboose is not ready yet.`
+            });
+        }
+    }
+    return caboose.getNextManager().getNextHandler();
+}
