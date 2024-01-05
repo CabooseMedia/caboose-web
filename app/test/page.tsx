9170abad3ba97cd0f6411d2a9776ad8922f5759f
Isaac Robbins <isaacprobbins@gmail.com>
2024-01-04 22:40:43 -0500

    testing

---
+++ app/test/page.tsx
@@ -0,0 +1,7 @@
+export default function TestPage() {
+    return (
+        <div className='fixed inset-0 w-full h-full'>
+            <h1>Hello from Test</h1>
+        </div>
+    )
+}
---
+++ app/web/page.tsx
@@ -0,0 +1,7 @@
+export default function WebPage() {
+    return (
+        <div className='fixed inset-0 w-full h-full'>
+            <h1>Hello from Web</h1>
+        </div>
+    )
+}
