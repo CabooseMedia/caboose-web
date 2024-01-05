05c6c8314a3e7808d16017eba71d6729e865dd49
Isaac Robbins <isaacprobbins@gmail.com>
2024-01-04 22:43:26 -0500

    remove testing

--- app/test/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function TestPage() {
-    return (
-        <div className='fixed inset-0 w-full h-full'>
-            <h1>Hello from Test</h1>
-        </div>
-    )
-}
--- app/web/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function WebPage() {
-    return (
-        <div className='fixed inset-0 w-full h-full'>
-            <h1>Hello from Web</h1>
-        </div>
-    )
-}
